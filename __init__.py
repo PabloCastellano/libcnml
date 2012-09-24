6aea001b97f2bfa55608d37b4589bdd7ce8fdc77
Pablo Castellano <pablo@anche.no>
2012-09-24 03:09:55 +0200

    Moved libcnml.py to its own directory and create __init__.py

---
+++ __init__.py
@@ -0,0 +1,23 @@
+# __init__.py - Guifi.net API handler
+# Copyright (C) 2012 Pablo Castellano <pablo@anche.no>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__version__ = '1.0'
+__author__ = 'Pablo Castellano <pablo@anche.no>'
+__license__ = 'GPLv3+'
+
+__all__ = ['libcnml'] 
+
+from libcnml import CNMLParser, Status
