d7058ec33e8a0cdbb85066b56a4a6e292ce8ca37
Pablo Castellano <pablo@anche.no>
2014-10-26 18:36:32 +0100

    [libcnml] Release version 0.8

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@
 
 from setuptools import setup
 
-VERSION = '0.7'
+VERSION = '0.8'
 
 setup(
     name='libcnml',
+    packages=['libcnml'],
     version=VERSION,
     description="A CNML parser for Python",
     long_description=open('README.txt').read(),
     author='Pablo Castellano',
     author_email='pablo@anche.no',
-    packages=['libcnml'],
+    url='https://github.com/PabloCastellano/guifinetstudio',
+    download_url='https://github.com/PabloCastellano/guifinetstudio/releases/tag/libcnml_0.8',
+    keywords = ['cnml', 'free networks', 'guifi.net'],
     license='GPLv3+',
     data_files=[('', ['LICENSE.txt'])],
     include_package_data=True,
