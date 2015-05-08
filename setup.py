e8f29cfa3af134b67e27069b7798244dafbdc299
Federico Capoano <nemesis@ninux.org>
2015-05-08 12:53:39 +0200

    Updated & unified VERSION

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
+import sys
 from setuptools import setup
 
-VERSION = '0.8'
+sys.path.insert(0, 'libcnml')
+from version import __version__, __license__
+sys.path.remove('libcnml')
 
 setup(
     name='libcnml',
     packages=['libcnml'],
-    version=VERSION,
+    version=__version__,
     description="A CNML parser for Python",
     long_description=open('README.md').read(),
     author='Pablo Castellano',
@@ -15,7 +18,7 @@ setup(
     url='https://github.com/PabloCastellano/libcnml/',
     download_url='https://github.com/PabloCastellano/libcnml/archive/master.zip',
     keywords = ['cnml', 'free networks', 'guifi.net'],
-    license='GPLv3+',
+    license=__license__,
     data_files=[('', ['LICENSE.txt'])],
     include_package_data=True,
     zip_safe=False,
